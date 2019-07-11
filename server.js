4d7e2c64eaacbcbfb4c36b3d0f0f8474750cb3cf
Glitch (oily-seaplane) <none>
2019-07-11 17:11:39 +0000

    Working on urlToShorten

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
@@ -14,6 +14,10 @@ var port = process.env.PORT || 3000;
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
 
+//use body-parser
+app.use(bodyParser.json());
+
+//app use cors
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
@@ -25,17 +29,18 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
 //Start API endpoint...
 app.get("/api/shorturl/new", function(req, res) {
   res.json({shorturl: 'This is your shorturl'});
 });
 
+//Creates the database entry
+app.get("/api/new/:urlToShorten(*)", function(req, res, next){
+  var { urlToShorten } = req.params;
+  console.log(urlToShorten);
+  res.json({urlToShorten: urlToShorten});
+});
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
